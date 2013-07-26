7738c5c4b99a07d1f4f4423f27f4315183bf0517
Sergio Barbosa <serarba@hotmail.com>
2013-07-25 21:59:48 -0500

    Added missing files.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+#*******************************************************************************
+#
+# home_controller.rb
+# Version 1.0a
+#
+# Description: 
+#
+# Revision History
+# ----------------
+# 25Jul13 - SergioBarbosa - Created controller.
+class HomeController < ActionController::Base
+   
+   def index
+   end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+London::Application.config.secret_token = 'c6329acc2d02257d58d800156bbdaf32b19add0cc4dc3b9be81e6522bea507757a163087a0745b9669d71a2f8525ba2256b59af23f957bf15dcff3fbfd7432c8'
